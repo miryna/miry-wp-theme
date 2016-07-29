12359719af98e673cf6e196914e6196570451ff2
miryna <m.iryna2010@gmail.com>
2016-07-29 16:27:44 +0300

    Initial commit. Theme-starter

---
+++ inc/back-compat.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * miry back compat functionality
+ *
+ * Prevents miry from running on WordPress versions prior to 4.4,
+ * since this theme is not meant to be backward compatible beyond that and
+ * relies on many newer functions and markup changes introduced in 4.4.
+ *
+ * @package WordPress
+ * @subpackage miry
+ * @since miry 1.0
+ */
+
+/**
+ * Prevent switching to miry on old versions of WordPress.
+ *
+ * Switches to the default theme.
+ *
+ * @since miry 1.0
+ */
+function miry_switch_theme() {
+	switch_theme( WP_DEFAULT_THEME, WP_DEFAULT_THEME );
+
+	unset( $_GET['activated'] );
+
+	add_action( 'admin_notices', 'miry_upgrade_notice' );
+}
+add_action( 'after_switch_theme', 'miry_switch_theme' );
+
+/**
+ * Adds a message for unsuccessful theme switch.
+ *
+ * Prints an update nag after an unsuccessful attempt to switch to
+ * miry on WordPress versions prior to 4.4.
+ *
+ * @since miry 1.0
+ *
+ * @global string $wp_version WordPress version.
+ */
+function miry_upgrade_notice() {
+	$message = sprintf( __( 'miry requires at least WordPress version 4.4. You are running version %s. Please upgrade and try again.', 'miry' ), $GLOBALS['wp_version'] );
+	printf( '<div class="error"><p>%s</p></div>', $message );
+}
+
+/**
+ * Prevents the Customizer from being loaded on WordPress versions prior to 4.4.
+ *
+ * @since miry 1.0
+ *
+ * @global string $wp_version WordPress version.
+ */
+function miry_customize() {
+	wp_die( sprintf( __( 'miry requires at least WordPress version 4.4. You are running version %s. Please upgrade and try again.', 'miry' ), $GLOBALS['wp_version'] ), '', array(
+		'back_link' => true,
+	) );
+}
+add_action( 'load-customize.php', 'miry_customize' );
+
+/**
+ * Prevents the Theme Preview from being loaded on WordPress versions prior to 4.4.
+ *
+ * @since miry 1.0
+ *
+ * @global string $wp_version WordPress version.
+ */
+function miry_preview() {
+	if ( isset( $_GET['preview'] ) ) {
+		wp_die( sprintf( __( 'miry requires at least WordPress version 4.4. You are running version %s. Please upgrade and try again.', 'miry' ), $GLOBALS['wp_version'] ) );
+	}
+}
+add_action( 'template_redirect', 'miry_preview' );
