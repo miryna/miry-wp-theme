0474be81ff1d9fd6bde4a2d442095445209a99f2
miryna <m.iryna2010@gmail.com>
2016-07-31 23:50:51 +0300

    Added slider for header Front page

--- header.php
+++ header.php
@@ -23,15 +23,28 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-		<!--  -->
-	<div class="site-inner">
+<!--	<div class="site-inner">  -->
 		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'miry' ); ?></a>
 
 		<header id="masthead" class="site-header" role="banner">
-			<div class="site-header-main">
-				<div class="site-branding">
-					<?php miry_the_custom_logo(); ?>
 
+            <?php if ( is_front_page() ) : ?>
+            <div class="site-header-image-front">
+
+                <!-- plugin [Slider Ultimate 1.0.1]-->
+                <?php echo do_shortcode("[ultimate-slider]"); ?>
+
+            </div><!--.site-header-image-front-->
+
+            <?php else : ?>
+            <div class="site-header-image" style="background: url('http://localhost/wp45/wp-content/uploads/2016/07/frontslider.jpg') no-repeat center center; height:400px;">
+
+            </div><!-- .site-header-image-->
+            <?php endif; ?>
+
+
+    		<div class="site-header-main content-width">
+    				<div class="site-branding ">
 					<?php if ( is_front_page() && is_home() ) : ?>
 						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<?php else : ?>
@@ -42,14 +55,12 @@
 					if ( $description || is_customize_preview() ) : ?>
 						<p class="site-description"><?php echo $description; ?></p>
 					<?php endif; ?>
-				</div><!-- .site-branding -->
+				    </div><!-- .site-branding -->
 
-		<!-- menu -->
-    			<?php if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) ) : ?>
+				<?php if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) ) : ?>
 					<button id="menu-toggle" class="menu-toggle"><?php _e( 'Menu', 'miry' ); ?></button>
 
 					<div id="site-header-menu" class="site-header-menu">
-                        <div class="section-inner">
 						<?php if ( has_nav_menu( 'primary' ) ) : ?>
 							<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary Menu', 'miry' ); ?>">
 								<?php
@@ -74,42 +85,12 @@
 								?>
 							</nav><!-- .social-navigation -->
 						<?php endif; ?>
-					    </div><!-- .section-inner -->
 					</div><!-- .site-header-menu -->
 				<?php endif; ?>
 			</div><!-- .site-header-main -->
 
 
+		</header><!--header -->
 
 
-			<?php if ( get_header_image() ) : ?>
-				<?php
-					/**
-					 * Filter the default miry custom header sizes attribute.
-					 *
-					 * @since miry 1.0
-					 *
-					 * @param string $custom_header_sizes sizes attribute
-					 * for Custom Header. Default '(max-width: 709px) 85vw,
-					 * (max-width: 909px) 81vw, (max-width: 1362px) 88vw, 1200px'.
-					 */
-				/*	$custom_header_sizes = apply_filters( 'miry_custom_header_sizes', '(max-width: 709px) 85vw, (max-width: 909px) 81vw, (max-width: 1362px) 88vw, 1200px' );
-				*/ ?>
-				<div class="header-image">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						<img src="<?php header_image(); ?>">
-                          <!--   srcset="<?php echo esc_attr( wp_get_attachment_image_srcset( get_custom_header()->attachment_id ) ); ?>"
-                             sizes="<?php echo esc_attr( $custom_header_sizes ); ?>"
-                             width="<?php echo esc_attr( get_custom_header()->width ); ?>"
-                             height="<?php echo esc_attr( get_custom_header()->height ); ?>"
-                             alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"> -->
-					</a>
-				</div><!-- .header-image -->
-			<?php endif; // End header image check. ?>
-
-
-
-
-		</header><!-- .site-header -->
-
-		<div id="content" class="site-content">
+        <div id="content" class="site-content">
